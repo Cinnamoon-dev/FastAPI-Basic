89a5f0e0100fc12b87649d86bd509ca0e48adf90
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-04 15:05:25 -0300

    feat: creating update and delete endpoints

--- app/routers/user.py
+++ app/routers/user.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/user")
 
+
 @router.get("/all")
 def userAll():
     db = get_db()
@@ -50,3 +51,57 @@ def userAdd(user: userSchema.UserSchema):
     except:
         db.rollback()
         return {"error": True, "message": "database error"}
+
+@router.put("/edit/{id:int}")
+def userEdit(id: int, user: userSchema.UserEditSchema):
+    db = get_db()
+
+    oldUser = db.query(userModel.User).filter(userModel.User.id == id).first()
+
+    if not oldUser:
+        return {"error": True, "message": "user not found"}, 404
+
+    if "email" in user.model_dump() and user.model_dump().get("email") is not None:
+        repeatedEmail = db.query(userModel.User).filter(userModel.User.email == user.model_dump().get("email")).first()
+
+        if repeatedEmail:
+            return {"error": True, "message": "Email already registered"}, 409
+
+    if "email" in user.model_dump() and user.model_dump().get("email") is not None:
+        setattr(oldUser, "email", user.model_dump().get("email"))
+    if "name" in user.model_dump() and user.model_dump().get("name") is not None:
+        setattr(oldUser, "name", user.model_dump().get("name"))
+    if "password" in user.model_dump() and user.model_dump().get("password") is not None:
+        setattr(oldUser, "password", user.model_dump().get("password"))
+
+    db.add(oldUser)
+
+    try:
+        db.flush()
+        db.commit()
+        return {"error": False, "message": "deu bom"}
+    
+    except:
+        db.rollback()
+        return {"error": True, "message": "database error"}
+
+@router.delete("/delete/{id:int}")
+def userView(id: int):
+    db = get_db()
+
+    user = db.query(userModel.User).filter(userModel.User.id == id).first()
+
+    if not user:
+        return {"error": True, "message": "user not found"}, 404
+    
+    db.delete(user)
+    
+    try:
+        db.flush()
+        db.commit()
+        return {"error": False, "message": "deu bom"}
+
+    except:
+        db.rollback()
+        return {"error": True, "message": "database error"}
+
