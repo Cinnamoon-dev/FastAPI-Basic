1709cf6fc86163c7004e5b2d7a3587dbe4406276
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-04 16:05:25 -0300

    refactor: adding checks and listing objects better

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,8 +1,7 @@
+from fastapi import APIRouter, Request
 from app.database import get_db
 from app.models import userModel
 from app.schemas import userSchema
-from fastapi import APIRouter, Request
-from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/user")
 
@@ -10,21 +9,23 @@ router = APIRouter(prefix="/user")
 @router.get("/all")
 def userAll():
     db = get_db()
-
     users = db.query(userModel.User).all()
     
     data = {
         "error": False,
-        "items": users
+        "items": []
     }
+
+    for user in users:
+        user_json = user.to_dict()
+        data["items"].append(user_json)
 
     return data
 
 @router.get("/view/{id:int}")
 def userView(id: int):
     db = get_db()
-
-    user = db.query(userModel.User).filter(userModel.User.id == id).first()
+    user = db.query(userModel.User).get(id)
 
     if not user:
         return {"error": True, "message": "user not found"}, 404
@@ -39,14 +40,17 @@ def userView(id: int):
 @router.post("/add")
 def userAdd(user: userSchema.UserSchema):
     db = get_db()
-    newUser = userModel.User(user.name, user.email, user.password)
-
+    
+    if db.query(userModel.User).filter(userModel.User.email == user.email.lower()).first():
+        return {"error": True, "message": "email already registered"}, 409
+    
+    newUser = userModel.User(user.name, user.email.lower(), user.password)
     db.add(newUser)
 
     try:
         db.flush()
         db.commit()
-        return {"error": False, "message": "deu bom"}
+        return {"error": False, "message": "email criado com sucesso"}
     
     except:
         db.rollback()
@@ -55,8 +59,7 @@ def userAdd(user: userSchema.UserSchema):
 @router.put("/edit/{id:int}")
 def userEdit(id: int, user: userSchema.UserEditSchema):
     db = get_db()
-
-    oldUser = db.query(userModel.User).filter(userModel.User.id == id).first()
+    oldUser = db.query(userModel.User).get(id)
 
     if not oldUser:
         return {"error": True, "message": "user not found"}, 404
@@ -69,8 +72,10 @@ def userEdit(id: int, user: userSchema.UserEditSchema):
 
     if "email" in user.model_dump() and user.model_dump().get("email") is not None:
         setattr(oldUser, "email", user.model_dump().get("email"))
+
     if "name" in user.model_dump() and user.model_dump().get("name") is not None:
         setattr(oldUser, "name", user.model_dump().get("name"))
+
     if "password" in user.model_dump() and user.model_dump().get("password") is not None:
         setattr(oldUser, "password", user.model_dump().get("password"))
 
@@ -88,7 +93,6 @@ def userEdit(id: int, user: userSchema.UserEditSchema):
 @router.delete("/delete/{id:int}")
 def userView(id: int):
     db = get_db()
-
     user = db.query(userModel.User).filter(userModel.User.id == id).first()
 
     if not user:
