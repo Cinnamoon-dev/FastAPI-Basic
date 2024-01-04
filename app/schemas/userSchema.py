66f8d6989c5f40da3d12ba48fe869cd5ee196999
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-04 15:05:10 -0300

    feat: creating edit schema

--- app/schemas/userSchema.py
+++ app/schemas/userSchema.py
@@ -1,9 +1,18 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class UserSchema(BaseModel):
     name: str
     email: str
     password: str
+
+    class Config:
+        orm_mode = True
+
+class UserEditSchema(BaseModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str]  = None
 
     class Config:
         orm_mode = True
