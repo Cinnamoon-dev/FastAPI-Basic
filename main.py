c9c7b051c6ce9a946443e129fe36720420e82076
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-09 16:33:59 -0300

    feat: adding mail router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from app.routers import user
+from app.routers import user, mail
 from app.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
@@ -8,6 +8,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(mail.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
