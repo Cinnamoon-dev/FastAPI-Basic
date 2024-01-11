5b792f213dd7968d4eda46405119c0299e3dd840
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-11 11:20:07 -0300

    feat: app reload enabled

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ app.include_router(user.router)
 app.include_router(mail.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
