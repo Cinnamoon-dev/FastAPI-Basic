59774a43e0700740346ddedab7a7d2e40f7c0276
PedroH183 <pedro_freires18@outlook.com>
2024-01-14 11:47:29 -0500

    feat: basic authentication implemented

--- app/routers/authRouter.py
+++ app/routers/authRouter.py
@@ -1,27 +1,70 @@
-import json
-from fastapi import APIRouter, Response
-from pydantic import BaseModel
-
-from app.database import get_db
-from app.models import userModel
-
-router = APIRouter(prefix="/auth")
-
-class Auth(BaseModel):
-    email : str
-    password : str
+from jose import jwt
+from starlette import status
+from app.models.userModel import User
+from datetime import timedelta, datetime
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import APIRouter,  HTTPException
+from app import bcrypt_context, ALGORITHM, SECRETY_KEY
+from app.dependencys import db_dependency, user_dependency, form_auth_dependency
 
 
-@router.post("/")
-async def auth( request : Auth ):
-    db = get_db()
-
-    users = db.query(userModel.User).filter( userModel.User.email == request.email ).first()
-
-    if users is None:
-        object_response = json.dumps({"message": "Usuário não encontrado", "error" : True})
-        return Response( status_code=404, content=object_response )
+router = APIRouter(prefix="/auth", tags=["auth"])
 
 
+@router.post("/login")
+async def login( db : db_dependency, data_form : form_auth_dependency ):
+    
+    user = authenticate_user(data_form.username, data_form.password, db)
+   
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"message": "Usuário não encontrado", "error": True}
+        )
 
-    return request
+    token = create_access_token(user.email, user.id, timedelta(minutes=20))
+    content_response = jsonable_encoder({"access_token" : token, "token_type": "bearer"})
+    
+    response = JSONResponse(
+        status_code=status.HTTP_202_ACCEPTED,
+        content=jsonable_encoder(content_response), 
+    )
+
+    return response
+
+
+@router.get("/me", status_code = status.HTTP_200_OK)
+async def get_user_credentials( user : user_dependency ):
+    
+    if user is None:
+        raise HTTPException(
+            detail="Authentication fail", 
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+    
+    response = JSONResponse(
+        content=user,
+        status_code=status.HTTP_200_OK,
+    )
+
+    return response
+
+
+def authenticate_user(email : str, password: str, db):
+    user : User = db.query(User).filter( User.email == email ).first()
+
+    if user is None:
+        return False
+    if not bcrypt_context.verify(password, user.password):
+        return False
+    
+    return user 
+
+
+def create_access_token( email: str, user_id : int, expires_time: timedelta ):
+    encode = {'email' : email, 'user_id' : user_id}
+    expires = datetime.utcnow() + expires_time
+    encode.update({'exp' : expires})
+    
+    return jwt.encode(encode, SECRETY_KEY, algorithm=ALGORITHM)
