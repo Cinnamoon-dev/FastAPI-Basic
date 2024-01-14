5f2577e203a456c2e487ef2ffb3283b2115ad423
PedroH183 <pedro_freires18@outlook.com>
2024-01-14 15:16:09 -0500

    feat: rename variables and update imports

--- app/dependencys/dependency.py
+++ app/dependencys/dependency.py
@@ -1,25 +1,25 @@
 from typing import Annotated
 from starlette import status
 from jose import JWTError, jwt
-from app import ALGORITHM, SECRETY_KEY
 from fastapi import Depends, HTTPException
+from app import ALGORITHM, JWT_ACCESS_SECRETY_KEY
 from fastapi.security import OAuth2PasswordBearer
 
 
 # função para obter o usuario logado, funciona como o decorator.
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/login")
 
-def get_current_user(  token : Annotated[str, Depends(oauth2_bearer)] ):
+def get_current_user( token : Annotated[str, Depends(oauth2_bearer)] ):
   try:
-    payload = jwt.decode(token, SECRETY_KEY, algorithms=[ALGORITHM])
+    payload = jwt.decode(token, JWT_ACCESS_SECRETY_KEY, algorithms=[ALGORITHM])
     email : str = payload.get("email")
     user_id : int = payload.get("user_id")
 
     if None in [email, user_id]:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail={"message": "Não foi possível encontrar o user", "error": True}
-        )    
+      raise HTTPException(
+          status_code=status.HTTP_401_UNAUTHORIZED,
+          detail={"message": "Não foi possível encontrar o user", "error": True}
+      )
     return {"email" : email, "user_id" : user_id}
   
   except JWTError:
