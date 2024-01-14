54c76aea95b17d989aac18e1ff020771cfc99e70
PedroH183 <pedro_freires18@outlook.com>
2024-01-14 11:46:53 -0500

    feat: create folder of dependencys

---
+++ app/dependencys/__init__.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+from app.database import get_db
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from .dependency import get_current_user
+from fastapi.security import  OAuth2PasswordRequestForm
+
+# dependencys
+
+db_dependency = Annotated[Session, Depends(get_db)]
+form_auth_dependency = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
---
+++ app/dependencys/dependency.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+from starlette import status
+from jose import JWTError, jwt
+from app import ALGORITHM, SECRETY_KEY
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+
+# função para obter o usuario logado, funciona como o decorator.
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(  token : Annotated[str, Depends(oauth2_bearer)] ):
+  try:
+    payload = jwt.decode(token, SECRETY_KEY, algorithms=[ALGORITHM])
+    email : str = payload.get("email")
+    user_id : int = payload.get("user_id")
+
+    if None in [email, user_id]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={"message": "Não foi possível encontrar o user", "error": True}
+        )    
+    return {"email" : email, "user_id" : user_id}
+  
+  except JWTError:
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail={"message": "Não foi possível encontrar o user", "error": True}
+    )
