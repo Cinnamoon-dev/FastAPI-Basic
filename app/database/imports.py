6ed165076551feea14abc5ef0b3cdfc100b36152
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-15 11:14:50 -0300

    feat: new import file created

---
+++ app/database/imports.py
@@ -0,0 +1,32 @@
+"""
+    Duplicando pois é necessário outro arquivo para importar as definições de dentro do package database.
+    O arquivo insertData.py não 'enxerga' o __init__.py, e os arquivos externos não conseguem 'enxergar' o imports.py,
+    logo os dois se fazem necessários.
+"""
+
+
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+USER = os.getenv("POSTGRES_USER", "postgres")
+PASSWORD = os.getenv("POSTGRES_PASSWORD", "1234")
+HOST = os.getenv("POSTGRES_HOST", "localhost")
+PORT = os.getenv("POSTGRES_PORT", 5432)
+DATABASE = os.getenv("POSTGRES_DB", "fastdb")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    def _get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    return next(_get_db())
