35ce769745cb9f32660ffc00dbe2e7a20609a4f7
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-16 12:30:17 -0300

    refactor: configs isolated in a single file

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,28 +1,14 @@
-from main import app
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from fastapi.testclient import TestClient
-from app.database import SQLALCHEMY_DATABASE_URL
+from tests.conftest import client_app, test_db
 
 
-client = TestClient(app)
-
-engine = create_engine( 
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False}
-)
-
-TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def override_get_db():
-    db = TestSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-def test_read_all():
-    response = client.get("/user/all")
+def test_read_all(client_app, test_db):
+    response = client_app.get("/user/all")
     assert response.is_success == True
     assert type(response.json()["itens"]) == list
+
+def test_create_user(client_app, test_db):
+    create_user = {"email": "test@email.com", "password": "1234", "name": "test"}
+    response = client_app.post("/user/add", json=create_user)
+    print(response.json())
+    assert response.is_success == True
+    assert response.json().get("error") == False
