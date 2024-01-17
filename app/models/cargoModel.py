8e181de0d28daaa3ef02af272ab24cf88d1ad20f
PedroH183 <pedro_freires18@outlook.com>
2024-01-16 22:14:44 -0500

    feat: implemented cargo model

---
+++ app/models/cargoModel.py
@@ -0,0 +1,20 @@
+from app.database import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+
+class Cargo( Base ):
+  """ Classe que representa cada cargo de um usuario no sistema  """
+  
+  __tablename__ = "cargo"
+
+  id = Column(Integer, primary_key=True)
+  nome = Column(String(255), nullable=False)
+
+  usuarios = relationship("User", backref="cargo", lazy=True)
+
+  def __init__( self, nome: str ):
+    self.nome = nome
+  
+  def __repr__( self ):
+    return f"<Controller ${self.id}, ${self.nome}"
