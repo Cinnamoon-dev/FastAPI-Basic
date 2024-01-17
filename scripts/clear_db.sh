ebb3f1b222ca99b9d8dfdf01b09c7a3523384ef5
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-17 14:19:04 -0300

    fix: droping database properly

--- scripts/clear_db.sh
+++ scripts/clear_db.sh
@@ -2,15 +2,16 @@
 
 readonly database="fastdb"
 readonly test_database="test_fastdb"
+readonly database_list=$(psql -U postgres -lqt 2> /dev/null || sudo -u postgres psql -lqt 2> /dev/null)
 
-psql -U postgres -lqt | grep $database &> /dev/null || sudo -u postgres psql -lqt | grep $database &> /dev/null
+echo $database_list | grep $database &> /dev/null
 if [ $? -eq 0 ]; then
     psql -U postgres -c "DROP DATABASE ${database}" || sudo -u postgres psql -c "DROP DATABASE ${database}"
 fi
 
 psql -U postgres -c "CREATE DATABASE ${database}" || sudo -u postgres psql -c "CREATE DATABASE ${database}"
 
-psql -U postgres -lqt | grep $test_database &> /dev/null || sudo -u postgres psql -lqt | grep $test_database &> /dev/null
+echo $database_list | grep $test_database &> /dev/null
 if [ $? -eq 0 ]; then
     psql -U postgres -c "DROP DATABASE ${test_database}" || sudo -u postgres psql -c "DROP DATABASE ${test_database}"
 fi
