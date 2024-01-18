430f6ed70beb264b5c3ac3d14d83f8c302d80bb2
PedroH183 <pedro_freires18@outlook.com>
2024-01-18 10:04:45 -0500

    docs : adding important fields

--- app/schemas/userSchema.py
+++ app/schemas/userSchema.py
@@ -5,8 +5,10 @@ class UserAddSchema(BaseModel):
     name: str
     email: str
     password: str
+    cargo_id : int
 
     model_config = {
+        "cargo_id" : 1,
         "name": "user",
         "email": "user@email.com",
         "password": "password"
@@ -16,8 +18,10 @@ class UserEditSchema(BaseModel):
     name: Optional[str] = None
     email: Optional[str] = None
     password: Optional[str]  = None
+    cargo_id : Optional[int] = None
 
     model_config = {
+        "cargo_id" : 1,
         "name": "user",
         "email": "user@email.com",
         "password": "password"
--- app/swagger_models/userResponses.py
+++ app/swagger_models/userResponses.py
@@ -1,12 +1,17 @@
 from pydantic import BaseModel
 from app.swagger_models.generalResponses import PaginationDoc
 
+class CargoDoc(BaseModel):
+    id : int
+    nome : str
+
 class UserDoc(BaseModel):
     id: int
     name: str
     email: str
     password: str
     isVerified: bool
+    cargo : CargoDoc
 
 class UserAllDoc(BaseModel):
     error: bool
