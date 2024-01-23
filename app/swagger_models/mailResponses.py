edb222b045c9d47aaa269afbfdef25cd18382fe8
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-23 15:04:43 -0300

    docs: creating new response model

--- app/swagger_models/mailResponses.py
+++ app/swagger_models/mailResponses.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel, EmailStr
 class ForgotPasswordDoc(BaseModel):
     email: EmailStr
     newPassword: str
+
+class SendVerifyEmailDocSuccess(BaseModel):
+    message: str
+    error: bool = False
