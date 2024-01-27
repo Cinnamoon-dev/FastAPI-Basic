d3afd28e53b5d357b08e16b40f092045300a8e35
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-01-27 18:20:50 -0300

    fix: adding missing semicolon

--- scripts/clear_db.sh
+++ scripts/clear_db.sh
@@ -6,14 +6,14 @@ readonly database_list=$(psql -U postgres -lqt 2> /dev/null || sudo -u postgres 
 
 echo $database_list | grep $database &> /dev/null
 if [ $? -eq 0 ]; then
-    psql -U postgres -c "DROP DATABASE ${database}" || sudo -u postgres psql -c "DROP DATABASE ${database}"
+    psql -U postgres -c "DROP DATABASE ${database};" || sudo -u postgres psql -c "DROP DATABASE ${database};"
 fi
 
-psql -U postgres -c "CREATE DATABASE ${database}" || sudo -u postgres psql -c "CREATE DATABASE ${database}"
+psql -U postgres -c "CREATE DATABASE ${database};" || sudo -u postgres psql -c "CREATE DATABASE ${database};"
 
 echo $database_list | grep $test_database &> /dev/null
 if [ $? -eq 0 ]; then
-    psql -U postgres -c "DROP DATABASE ${test_database}" || sudo -u postgres psql -c "DROP DATABASE ${test_database}"
+    psql -U postgres -c "DROP DATABASE ${test_database};" || sudo -u postgres psql -c "DROP DATABASE ${test_database};"
 fi
 
-psql -U postgres -c "CREATE DATABASE ${test_database}" || sudo -u postgres psql -c "CREATE DATABASE ${test_database}"
+psql -U postgres -c "CREATE DATABASE ${test_database};" || sudo -u postgres psql -c "CREATE DATABASE ${test_database};"
